1b05d6fbeea80c35025506eed2daa4bfa751f0a0
P3TERX <25927179+P3TERX@users.noreply.github.com>
2022-02-15 16:20:26 +0800

    Update LICENSE

--- warp.sh
+++ warp.sh
@@ -7,7 +7,7 @@
 #
 # MIT License
 #
-# Copyright (c) 2021 P3TERX <https://p3terx.com>
+# Copyright (c) 2021-2022 P3TERX <https://p3terx.com>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
